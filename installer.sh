27859ee3da90fd2917300f5a5aa9ec0c74fc3c86
ParamagicDev <konnor5456@gmail.com>
2020-07-13 18:11:45 -0400

    added volumes

--- installer.sh
+++ installer.sh
@@ -13,7 +13,7 @@ command -v git || (echo "git executable not found" && exit 1)
 tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
 
 repo_name="bridgetown-automation-docker-compose"
-repo_url="https://github.com/ParamagicDev/$repo_name.git"
+repo_url="https://github.com/ParamagicDev/$repo_name"
 
 # Pull down files related to docker.
 git clone "$repo_url" "$tmp_dir"
@@ -42,27 +42,28 @@ printf "Successfully built your image for Bridgetown.\n\n"
 [ -z "$DESTINATION" ] && printf "What is the directory of your bridgetown project?\n" && read DESTINATION
 
 while true; do
-  [ -z "$PROJECT_TYPE" ] || break
+  [ "$PROJECT_TYPE" = "existing" ] || break
+  [ "$PROJECT_TYPE" = "new" ] || break
   printf "Is this for a new or existing Bridgetown project? [(N)ew, (E)xisting]\n"
   read PROJECT_TYPE
 
   # make case matching insensitive
   shopt -s nocasematch
-  if [ "$PROJECT_TYPE" == "existing" ] || [ "$PROJECT_TYPE" == "e" ]; then
+  if [ "$PROJECT_TYPE" = "existing" ] || [ "$PROJECT_TYPE" = "e" ]; then
     PROJECT_TYPE="existing"
     break
-  elif [ "$PROJECT_TYPE" == "new" ] || [ "$PROJECT_TYPE" == "n" ]; then
+  elif [ "$PROJECT_TYPE" = "new" ] || [ "$PROJECT_TYPE" = "n" ]; then
     PROJECT_TYPE="new"
     break
   fi
 done
 
-if [ "$PROJECT_TYPE" == "new" ]; then
-  docker run --rm -it "$docker_tag" bash -c "gem install bridgetown && \
+if [ "$PROJECT_TYPE" = "new" ]; then
+  docker run --rm -v ".:$APP_DIR" -it "$docker_tag" bash -c "gem install bridgetown && \
              bridgetown new $DESTINATION --apply=$repo_url"
-elif [ "$PROJECT_TYPE" == "existing" ]; then
+elif [ "$PROJECT_TYPE" = "existing" ]; then
   cd "$DESTINATION" || (echo "Unable to locate directory." && exit 1)
-  docker run --rm -it "$docker_tag" "bundle exec bridgetown apply $repo_url"
+  docker run --rm -v ".:$APP_DIR" -it "$docker_tag" "bundle exec bridgetown apply $repo_url"
 fi
 
 printf "Successfully added Docker to your bridgetown project\n"
