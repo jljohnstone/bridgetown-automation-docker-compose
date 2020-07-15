9d5893b19876469ba318da409d9b915a7a74ae2a
ParamagicDev <konnor5456@gmail.com>
2020-07-15 18:17:34 -0400

    good work, idk whats going on...

--- installer.sh
+++ installer.sh
@@ -27,22 +27,12 @@ shopt -u nocasematch
 source "$tmp_dir/docker.env"
 docker_tag="bridgetown-automation-docker:latest"
 
-printf "Building your docker image...\n\n"
-source "$tmp_dir/docker.env"
-docker build  -t $docker_tag \
-              -f $tmp_dir/Dockerfile \
-              --build-arg DOCKER_USER \
-              --build-arg USER_ID \
-              --build-arg GROUP_ID \
-              --build-arg APP_DIR \
-              "$tmp_dir"
 
-# Clean up
-rm -rf "$tmp_dir"
 
 printf "Successfully built your image for Bridgetown.\n\n"
 
 [ -z "$DESTINATION" ] && printf "What is the directory of your bridgetown project?\n" && read DESTINATION
+
 
 while true; do
   [ "$PROJECT_TYPE" = "existing" ] && break
@@ -61,32 +51,30 @@ while true; do
   fi
 done
 
+
+mkdir -p "$DESTINATION" || (echo "Unable to create new directory" && exit 1)
+cp "$tmp_dir/Gemfile"
+
+printf "Building your docker image...\n\n"
+source "$tmp_dir/docker.env"
+docker build  -t $docker_tag \
+              -f $tmp_dir/Dockerfile \
+              --build-arg DOCKER_USER \
+              --build-arg USER_ID \
+              --build-arg GROUP_ID \
+              --build-arg APP_DIR \
+              "$DESTINATION"
+
 if [ "$PROJECT_TYPE" = "new" ]; then
-  mkdir -p "$DESTINATION" || (echo "Unable to create new directory" && exit 1)
-
-  # docker run -v $(pwd):$APP_DIR-u $(id -u $USER):$(id -g $USER) --name bridgetown-docker -it "$docker_tag" bash -c "\
-  #   bundle exec bridgetown new $DESTINATION --apply=$repo_url --force
-  #   cp -a $DESTINATION $APP_DIR
-  # "
   docker rm -f bridgetown-docker || echo ""
-  docker run -u $(id -u $USER):$(id -g $USER) --name bridgetown-docker -it "$docker_tag" bash -c "\
-    bundle exec bridgetown new . --apply=\"$repo_url\" --force
-    echo \"
-
-    Performing clean up operations...\"
-    rm -rf node_modules
-  "
-  docker cp bridgetown-docker:"$APP_DIR" "$DESTINATION"
-  cp -R $DESTINATION/bridgetown/* $DESTINATION/bridgetown/.* $DESTINATION
-  rm -rf $DESTINATION/bridgetown
-  docker rm -f bridgetown-docker || echo ""
+  docker run -v "$(realpath $DESTINATION)":"$APP_DIR" \
+             -u $(id -u $USER):$(id -g $USER) -it "$docker_tag" \
+             bash -c "bundle exec bridgetown new . --apply=\"$repo_url\" --force"
 
 elif [ "$PROJECT_TYPE" = "existing" ]; then
   cd "$DESTINATION" || (echo "Unable to locate directory." && exit 1)
   docker run --rm -v "$(pwd):$APP_DIR" -it "$docker_tag" "bundle exec bridgetown apply $repo_url"
 fi
-
-
 
 printf "Successfully added Docker to your bridgetown project\n"
 printf "To use docker in your new project simply do the following:\n\n"
